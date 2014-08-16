d7259dc6982e51c1114df3c37f983d84037ad37c
Istivan Spiteri <istivan@gmail.com>
2014-08-16 13:56:20 +0200

    cleaned files a bit -- added vagrant reboot plugin

--- vagrant/files/gae.sh
+++ vagrant/files/gae.sh
@@ -26,53 +26,8 @@ wget -O /tmp/gae.zip https://storage.googleapis.com/appengine-sdks/featured/goog
 unzip /tmp/gae.zip -d /opt/
 
 # add the sdk install directory to on the system path
-# export PATH="$PATH:/opt/google_appengine"
-
-# make sure imp folders are on the system path:
-# export PATH="$PATH:/bin:/sbin:/usr/bin:/usr/local/bin:/usr/sbin"
-
-# add the sdk install directory to on the system path
 # and make sure imp folders are on the system path:
 export PATH="$PATH:/opt/google_appengine:/bin:/sbin:/usr/bin:/usr/local/bin:/usr/sbin"
-
-# # #
-GAE_APP_NAME=pw00
-# # cd ~
-# mkdir -p vagrant/app00
-# mkdir -p vagrant/app00/static
-#
-# echo """application: ${GAE_APP_NAME}
-# version: development
-# runtime: python27
-# api_version: 1
-# threadsafe: true
-#
-# default_expiration: 7d
-#
-# handlers:
-# - url: /static
-#   static_dir: static
-# - url: .*
-#   script: wsgi_app.app
-# """ > app00/app.yaml
-#
-#
-# echo """import webapp2
-#
-# class MainPage(webapp2.RequestHandler):
-#   def get(self):
-#       self.response.headers['Content-Type'] = 'text/plain'
-#       self.response.out.write('Please replace me with a decent WSGI App Framework such as Flask')
-#
-# app = webapp2.WSGIApplication([('/', MainPage)],
-#                               debug=True)
-# """ > app00/wsgi_app.py
-#
-# # testing the mini app engine
-# python dev_appserver.py gae_app/
-# python ./google_appengine/dev_appserver.py gae_app/
-# # #
-
 
 # # #
 # add a handy alias so users can run the sdk with sensible defaults
